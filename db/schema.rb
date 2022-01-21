a0f82a4283664963af4f206895686b0ba6af03b2
Marco <gdmarav374@gmail.com>
2022-01-22 00:01:50 +0100

    Create table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_221130) do
+ActiveRecord::Schema.define(version: 2022_01_21_225909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 2022_01_21_221130) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["recipe_id"], name: "index_tags_on_recipe_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "handle"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "ingredients", "recipes"
